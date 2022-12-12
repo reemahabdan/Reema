5de9f3ccd32c02bd787baf44a8e05c1ad777520c
Reema <r.alhabdan@live.com>
2022-12-12 16:48:32 +0300

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by Reema Alhabdan on 12/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
